f340f78a8759f80373f44e1af3186b63c3530155
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-26 01:32:50 -0700

    Update: Update card details.

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -2,21 +2,27 @@
 function CardDetails({ card }) {
   return (
     <div className="flex gap-5">
-      
+
       <img src={card.card_images[0].image_url_small} />
 
       <p>
-        archetype: <b>{card.archetype}</b><br />
-        atk: <b>{card.atk}</b><br />
-        attribute: <b>{card.attribute}</b><br />
-        def: <b>{card.def}</b><br />
-        desc: <b>{card.desc.replace("''", '').replace("''", '')}</b><br />
-        frameType: <b>{card.frameType}</b><br />
-        id: <b>{card.id}</b><br />
-        level: <b>{card.level}</b><br />
-        name: <b>{card.name}</b><br />
-        race: <b>{card.race}</b><br />
-        type: <b>{card.type}</b><br />
+        <div className="tm-title-1 mb-2"><b>{card.name}</b></div>
+        <div className="mb-2">
+          {card.atk && <span>Atk: <b>{card.atk}</b> &nbsp;</span>}
+          {card.def && <span>Def: <b>{card.def}</b> &nbsp;</span>}
+          {card.level && <span>Level: <b>{card.level}</b> &nbsp;</span>}
+          {card.attribute && <span>Attribute: <b>{card.attribute}</b></span>}
+        </div>
+
+        <div className="mb-2">
+          {card.type && <span>Type: <b>{card.type}</b> &nbsp;</span>}
+          {card.race && <span>Race: <b>{card.race}</b> &nbsp;</span>}
+          {card.archetype && <span>Arche Type: <b>{card.archetype}</b> &nbsp;</span>}
+          {card.frameType && <span>Frame Type: <b>{card.frameType}</b></span>}
+        </div>
+        Description: <br />
+
+        <b>{card.desc.replace("''", '').replace("''", '')}</b><br />
       </p>
 
     </div>
