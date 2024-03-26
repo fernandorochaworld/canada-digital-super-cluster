765fa53a1abb5af204e6ce5d35e37ad78c5c0ebf
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-26 00:01:03 -0700

    Update: Add search by race.

--- src/services/yugioh-service.js
+++ src/services/yugioh-service.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 export const getCardByName = (name) => {
     const url = `${import.meta.env.VITE_API_URL}?fname=${name}`;
-    const request = axios.post(url, name);
+    const request = axios.get(url);
+    return request.then(response => response.data);
+}
+
+export const getCardByRace = (race) => {
+    const url = `${import.meta.env.VITE_API_URL}?race=${race}`;
+    const request = axios.post(url);
     return request.then(response => response.data);
 }
