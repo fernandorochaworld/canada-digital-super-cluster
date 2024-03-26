543005818de88906484e2f97783d32b98d9d1872
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-26 00:02:02 -0700

    Update: Update search page.

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Button from "../components/Button";
+import { getCardByRace } from "../services/yugioh-service";
+import { DNA } from "react-loader-spinner";
 
 const race = {
   monster: [
@@ -45,29 +48,78 @@ const race = {
 
 
 function Page2() {
-  // const [count, setCount] = useState(0)
+  const [cardList, setCardList] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = (race) => {
+    setLoading(true);
+    getCardByRace(race).then(data => {
+      // console.log(data)
+      if (data.data.length) {
+        setCardList(data.data);
+      }
+      setLoading(false);
+    })
+      .catch(error => {
+        alert('No card matching your query was found.');
+        setLoading(false);
+      });
+  }
 
   return (
-    <div>
-      Monsters:
-      <div className="flex item-center flex-wrap gap-2">
-        {race.monster.map(item => (
-          <Button title={item}></Button>
-        ))}
+    <>
+      {
+        loading &&
+        <DNA
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      }
+      <div className="flex item-center flex-wrap gap-2 mb-3">
+
+        <div>
+          Monsters:
+          <div className="flex item-center flex-wrap gap-2">
+            {race.monster.map(item => (
+              <Button title={item} onClick={() => handleClick(item)}></Button>
+            ))}
+          </div>
+        </div>
+        <div>
+          Spell Cards:
+          <div className="flex item-center flex-wrap gap-2">
+            {race.spell.map(item => (
+              <Button title={item} onClick={() => handleClick(item)}></Button>
+            ))}
+          </div>
+        </div>
+        <div>
+          Trap Cards:
+          <div className="flex item-center flex-wrap gap-2">
+            {race.trap.map(item => (
+              <Button title={item} onClick={() => handleClick(item)}></Button>
+            ))}
+          </div>
+        </div>
       </div>
-      Spell Cards:
-      <div className="flex item-center flex-wrap gap-2">
-        {race.spell.map(item => (
-          <Button title={item}></Button>
-        ))}
-      </div>
-      Trap Cards:
-      <div className="flex item-center flex-wrap gap-2">
-        {race.trap.map(item => (
-          <Button title={item}></Button>
-        ))}
-      </div>
-    </div>
+      {
+        cardList && (
+          <div className="flex flex-wrap justify-items-center">
+            {cardList.map(card => (
+              <img
+                src={card.card_images[0].image_url_small}
+                style={{ width: '134px', height: '180px' }}
+                />
+              // width: '268px', height: '361px'
+            ))}
+          </div>
+        )
+      }
+    </>
   )
 }
 
