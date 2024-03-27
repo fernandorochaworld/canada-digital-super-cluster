270458bd83aad8f3a2f6ead8ca52e666def78e5c
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-26 22:03:31 -0700

    New: Upload Favorite Books Initial Project.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# canada-digital-super-cluster
+# Yu-Gi-Oh Card Search
+<img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" width="150px"/>
+
+## Description
+Use of Yu-Gi-Oh Cards API
+
+## Setup
+Create a .env file at the project root based on the .env.sample file attached to the project.
+
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run dev
+```
+
+
+## Live Website
+
+https://canada-digital-super-cluster.onrender.com
+
+
+## Screenshot
+
+<img src="./doc/Yu-Gi-Oh-Card-Search.png" width="600px"/>
+
+
+## License
+
+[MIT licensed](LICENSE).
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  Link,
+  // Link
+} from 'react-router-dom'
+
+import { useState } from 'react'
+import './App.css'
+import CardSearch from './pages/CardSearch'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <div className='flex flex-col h-full' style={{ minHeight: '100vh' }}>
+        <Navbar />
+        <div className='flex flex-col items-center flex-grow'>
+
+          <div className="max-w-7xl px-2 pt-2 pb-8 sm:px-6 lg:px-8 w-full">
+
+            <h1 className="tm-title-1 mb-2">
+              Favorite Books
+            </h1>
+
+
+            <Routes>
+              {
+                <>
+                  <Route path="/" element={<CardSearch />} />
+                  {/* <Route path="/task-list/:id?" element={<TaskList />} />
+                  <Route path="/task-list/:taskListId/task/:taskId" element={<Task />} /> */}
+                </>
+              }
+            </Routes>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
