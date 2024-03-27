412125c1fcacb152714ad9f00cec2c94403e0566
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-26 22:31:44 -0700

    New: Add Book API Service.

---
+++ src/services/book-service.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const getAll = () => {
+    const url = `${import.meta.env.VITE_API_URL}/api/favorite-books/books`;
+    const request = axios.get(url);
+    return request.then(response => response.data);
+}
+export const getBookByName = (name) => {
+    const url = `${import.meta.env.VITE_API_URL}/api/favorite-books/books`;
+    const request = axios.get(url);
+    return request.then(response => response.data);
+}
+
+export const getBookByRace = (race) => {
+    const url = `${import.meta.env.VITE_API_URL}?race=${race}`;
+    const request = axios.post(url);
+    return request.then(response => response.data);
+}
